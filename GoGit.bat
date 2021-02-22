f6db67221a1ac234934f47b8b3cc3bd7b296ebe2
Colgate13x <gabreilbarros13@gmail.com>
2021-02-22 11:11:59 -0300

    Push by GoGit -> NLW 1

--- GoGit.bat
+++ GoGit.bat
@@ -5,6 +5,6 @@ git add yarn.lock
 git add tsconfig.json
 git add node.txt
 git commit -m "Push by GoGit -> NLW 1"
-git pull https://github.com/Colgate13/AppPortifolio main
-git push https://github.com/Colgate13/AppPortifolio -u main
+git pull https://github.com/Colgate13/NLW4---Nodejs.git main
+git push https://github.com/Colgate13/NLW4---Nodejs.git -u main
 git status
